e4ea2a192eabb24d04b297c436878e234b90b253
sebuszqo <sekn.kn@gmail.com>
2023-03-05 15:56:00 +0100

    Added basic structure of frontend main view right now with css.

--- map-market/src/App.tsx
+++ map-market/src/App.tsx
@@ -4,23 +4,23 @@ import {FaSearchLocation} from "react-icons/fa";
 // import {FaSearchLocation} from "react-icons/all";
 
 export const App = () => {
-    return (
-        <>
-            <div className="wrapper">
-                <header>
-                    <h1>
-                        <strong>Map</strong> Market
-                    </h1>
-                    <button>New announcement</button>
-                    <div className="search">
-                        <input type="text"/>
-                        <button>
-                            <FaSearchLocation/>
-                        </button>
-                    </div>
-                </header>
-                <div className="map"></div>
+  return (
+      <>
+        <div className="wrapper">
+          <header>
+            <h1>
+              <strong>Map</strong> Market
+            </h1>
+            <button>New announcement</button>
+            <div className="search">
+              <input type="text"/>
+              <button>
+                <FaSearchLocation/>
+              </button>
             </div>
-        </>
-    );
+          </header>
+          <div className="map"></div>
+        </div>
+      </>
+  );
 };
