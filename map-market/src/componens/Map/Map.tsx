7827c6f2400b9f1517b1c33cfc461df9b6efb320
sebuszqo <sekn.kn@gmail.com>
2023-03-06 22:33:59 +0100

    Added map to the app with leaflet react components and I used OpenStreetMaps as provider of map.

--- map-market/src/componens/Map/Map.tsx
+++ map-market/src/componens/Map/Map.tsx
@@ -1,5 +1,20 @@
 import "./Map.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "../../utils/fix-map-icon";
 
 export const Map = () => {
-  return <div className="map"></div>;
+  return (
+    <div className="map">
+      <MapContainer center={[50.0688456, 19.9143048]} zoom={15}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https://www.openstreetmap.org/opyright'>OpenStreetMap</a> & contributors"
+        />
+        <Marker position={[50.0688456, 19.9143048]}>
+          <Popup>Main view</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
