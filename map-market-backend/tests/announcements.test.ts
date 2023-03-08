ccda09e7e31a93b5ea1099dd1fd601dc2cb907fa
sebuszqo <sekn.kn@gmail.com>
2023-03-08 16:02:26 +0100

    Added TDD tests to announcement record to start with this piece of backend.

---
+++ map-market-backend/tests/announcements.test.ts
@@ -0,0 +1,20 @@
+// TDD for static methods
+
+import {AnnounceRecord} from "../records/announce.record";
+
+test("Announcement records returns data from database for one entry.", async () => {
+  const announcement = await AnnounceRecord.findOne("abc");
+  expect(announcement).toBeDefined();
+  expect(announcement.id).toBe("abc");
+  expect(announcement.name).toBe("Testowe");
+  expect(announcement.price).toBe(22);
+  expect(announcement.description).toBe("Test description");
+  expect(announcement.latitude).toBe(50.0688456);
+  expect(announcement.longitude).toBe(19.9143048);
+  expect(announcement.url).toBe("url");
+});
+
+test("Announcement records returns null from database for entry that not exists.", async () => {
+  const announcement = await AnnounceRecord.findOne("123");
+  expect(announcement).toBeNull();
+});
