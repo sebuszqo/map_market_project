2f6bdb8cf3ceec0747d5a118409e9cabc2e4b063
sebuszqo <sekn.kn@gmail.com>
2023-03-08 17:11:46 +0100

    Added, context to search bar and added first part of search feature to react app.

--- map-market/src/componens/layout/Header.tsx
+++ map-market/src/componens/layout/Header.tsx
@@ -1,24 +1,38 @@
-import {FaSearchLocation} from "react-icons/fa";
+import { FaSearchLocation } from "react-icons/fa";
 import "./Header.css";
-import {Btn} from "../common/Btn";
+import { Btn } from "../common/Btn";
+import { FormEvent, useContext, useState } from "react";
+import { SearchContext } from "../../context/search.context";
 
 export const Header = () => {
-    return (
-        <header>
-            <h1>
-                <strong>Map</strong> Market
-            </h1>
-            <Btn text={"New announcement"}/>
-            <div className="search">
-                <input type="text"/>
-                <Btn
-                    text={
-                        <p id={"icon"}>
-                            <FaSearchLocation/>
-                        </p>
-                    }
-                />
-            </div>
-        </header>
-    );
+  const { setSearch, search } = useContext(SearchContext);
+  const [inputVal, setInputVal] = useState(search);
+
+  const setSearchFromLocalState = (e: FormEvent) => {
+    e.preventDefault();
+    setSearch(inputVal);
+  };
+
+  return (
+    <header>
+      <h1>
+        <strong>Map</strong> Market
+      </h1>
+      <Btn text={"New announcement"} />
+      <form className="search" onSubmit={setSearchFromLocalState}>
+        <input
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <Btn
+          text={
+            <p id={"icon"}>
+              <FaSearchLocation />
+            </p>
+          }
+        />
+      </form>
+    </header>
+  );
 };
